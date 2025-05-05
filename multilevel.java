b5c0452b9703c7638feabd8d83b72840d1eb9b1d
AnushAdk04 <adhikarianush64@gmail.com>
2025-05-05 13:31:14 +0545

    multilevel inheritance

---
+++ multilevel.java
@@ -0,0 +1,38 @@
+// WAP to show multilevel inheritance in java 2081-01-22
+
+class person{
+    int code;
+    String name;
+    void display(){
+        System.out.println("code: "+code);
+        System.out.println("Name: "+name);
+    }
+}
+
+class staff extends person{
+    String company;
+    void display(){
+        super.display();
+        System.out.println("Company: "+company);
+    }
+    
+}
+
+class Manager extends staff{
+    String department;
+    void display(){
+        super.display();
+        System.out.println("Department: "+department);
+    }
+}
+
+public class MultiLevel{
+    public static void main(String args[]){
+        Manager m = new Manager();
+        m.code = 10;
+        m.name = "Anush Adhikari";
+        m.company = "Tech Corp";
+        m.department = "IT";
+        m.display();
+    }
+}
