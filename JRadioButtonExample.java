94b4e029b35c7821efb15fee449e607ede5eeb17
AnushAdk04 <adhikarianush64@gmail.com>
2025-05-26 11:32:37 +0545

    menu bar and action events

--- JRadioButtonExample.java
+++ JRadioButtonExample.java
@@ -2,15 +2,18 @@
 // 2082-02-05
 
 import javax.swing.*;
-public class JRadioButtonExample{
+
+public class JRadioButtonExample {
+
     JFrame f;
-    JRadioButtonExample(){
+
+    JRadioButtonExample() {
         f = new JFrame();
 
         JRadioButton r1 = new JRadioButton("Male");
         JRadioButton r2 = new JRadioButton("Female");
-        r1.setBounds(75,50,100,30);
-        r2.setBounds(75,100,100,30);
+        r1.setBounds(75, 50, 100, 30);
+        r2.setBounds(75, 100, 100, 30);
 
         ButtonGroup bg = new ButtonGroup();
 
@@ -19,13 +22,13 @@ public class JRadioButtonExample{
         f.add(r1);
         f.add(r2);
 
-        f.setSize(300,300);
+        f.setSize(300, 300);
         f.setLayout(null);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new JRadioButtonExample();
     }
 }
--- palindrome.java
+++ palindrome.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
-class palindrome{
-    public static void main(String[]  args){
-        int i,n,c=0,r,rev=0;
+
+class palindrome {
+    public static void main(String[] args) {
+        int i, n, c = 0, r, rev = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         n = sc.nextInt();
         int temp = n;
-        while(n>0){
-            r = n%10;
-            rev = rev*10+r;
-            n = n/10;
+        while (n > 0) {
+            r = n % 10;
+            rev = rev * 10 + r;
+            n = n / 10;
         }
-        if(temp==rev){
+        if (temp == rev) {
             System.out.println("The number is a palindrome.");
-        }
-        else{
+        } else {
             System.out.println("The number is not a palindrome.");
         }
     }
